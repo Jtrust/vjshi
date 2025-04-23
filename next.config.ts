59d6bf864e3d3b856eeef33f06c9aabf171356a4
Zhang Juntao <zhangjuntao@ZhangdeMacBook-Pro.local>
2025-04-24 01:15:47 +0800

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath,
+  assetPrefix
 };
 
 export default nextConfig;
